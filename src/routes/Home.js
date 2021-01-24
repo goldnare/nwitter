7e4a7f55f1962aee486112eb65f6f5b51960e6a2
pmk <goldnare@naver.com>
2021-01-24 13:25:19 +0900

    Help...

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,41 @@
+import Nweet from "components/Nweet";
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
+const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+
+    //아래 주석은 구 방식이라고 함
+    // const getNweets = async() =>{
+    //     const dbNweets = await dbService.collection("nweets").get();
+    //     dbNweets.forEach(document => {
+    //         const nweetObject = {
+    //             ...document.data(),
+    //             id: document.id,
+    //         };
+    //         setNweets((prev) => [nweetObject, ...prev]);
+    //     });
+    // }
+    
+    useEffect(() => {
+        // getNweets(); //위에 주석과 연관된 방식
+
+        //아래 방식으로 사용하면 re-render 를 더 적게 하기떄문에 속도가 더 빠른다고함
+        dbService.collection("nweets").onSnapshot((snapshot) => {
+            const nweetArray = snapshot.docs.map((doc) => ({
+                id:doc.id, 
+                ...doc.data(),
+            }));
+            setNweets(nweetArray);
+        });
+    }, [])
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
-            nweet,
-            createAt: Date.now(),
+            text: nweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid,
         });
         setNweet("");
     };
@@ -22,6 +50,11 @@ const Home = () => {
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <Nweet key={nweets.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
+                ))}
+            </div>
         </div>
     );
     
